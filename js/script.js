e59a7d8525e2c5fa36ad069e388cd1af3b4c25db
Marco Pacheco <79469633+marcopacheco-dev@users.noreply.github.com>
2025-04-03 13:22:18 -0300

    adicionando carrosel com tres imagens

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 function openWhatsApp() {
     window.open('https://wa.me/552175782947', '_blank');
 }
+
+// Função para Carrosel
+let currentSlide = 0;
+function moveSlide(step) {
+    const slides = document.querySelectorAll(".carousel-slide");
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + step + slides.length) % slides.length;
+    slides[currentSlide].style.display = "block";
+}
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".carousel-slide")[currentSlide].style.display = "block";
+});
